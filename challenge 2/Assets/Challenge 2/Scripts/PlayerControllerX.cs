deeb5336e4a08206dda3bd41959e866ac5b74c26
David <David_dekker@live.nl>
2022-04-06 15:37:54 +0200

    challenge 2

---
+++ challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 1.0f;
+    public float waitTime = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update timer
+        timer += Time.deltaTime;
+        // On spacebar press and timer is bigger than wait time, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (timer >= waitTime)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0;
+            }
+        }
+    }
+}
